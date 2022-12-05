c38d2688fcb663c4b8cbffd97e6ac358d014f2a2
BrookeHeck <b19heck@gmail.com>
2022-12-04 20:13:07 -0800

    add queue methods

--- src/dataStructures/stack/Stack.java
+++ src/dataStructures/stack/Stack.java
@@ -21,23 +21,42 @@ public class Stack {
     }
 
     public Boolean isEmpty() {
-
+        if(this.top == null) return true;
+        return false;
     }
 
     public Node peek() {
-
+        if(!isEmpty()) return this.head.value;
+        return null;
     }
 
     public void push(String value) {
-
+        Node newNode = new Node(value);
+        if(isEmpty()) this.head = newNode;
+        else {
+            newNode.changeNext(this.head);
+            this.head = newNode;
+        }
     }
 
     public Node pop() {
-
+        if(isEmpty()) return null;
+        else {
+            Node temp = this.head;
+            this.head = this.head.next;
+            return temp.value;
+        }
     }
 
     @Override
     public String toString() {
-        
+        Node current = this.head;
+        String str = "TOP ";
+        while(current != null) {
+            str = str + current.value + " -> ";
+            current = current.next;
+        }
+        str += "BOTTOM";
+        return str;
     }
 }
