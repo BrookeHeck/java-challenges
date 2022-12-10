20b1b212e71a226813c21f150bc5d9cc2a3ce393
BrookeHeck <b19heck@gmail.com>
2022-12-10 12:47:42 -0800

    fix the hashmap class

--- src/dataStructures/hashmap/HashMap.java
+++ src/dataStructures/hashmap/HashMap.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class HashTable {
+public class HashMap {
     int size;
-    ArrayList table;
-    public HashTable(int size) {
+    ArrayList<LinkedList<String>> table;
+
+    public HashMap(int size) {
         this.size = size;
-        this.table = new ArrayList<LinkedList>();
+        this.table = new ArrayList<>();
     }
 
     private int hash(String key) {
@@ -15,34 +17,34 @@ public class HashTable {
                 sum += key.codePointAt(key.charAt(i));
             }
             return (sum * 599 % this.size);
-        } catch(e) {
+        } catch(Exception e) {
             System.out.println(e);
-            return null;
+            return -1;
         }
     }
 
-    public void set(Stirng key, String value) {
+    public void set(String key, String value) {
         int hash = this.hash(key);
 
-        if(hash != null) {
-            LinkedList keyList = this.table.get(hash);
-            if(list != null) {
+        if(hash != -1) {
+            LinkedList<String> keyList = this.table.get(hash);
+            if(keyList != null) {
                 keyList.add(key + ':' + value);
             } else {
-                LinkedList ll = new LinkedList<String>();
-                this.table.set(hash, ll);
+                LinkedList<String> ll = new LinkedList<>();
+                this.table.add(hash, ll);
             }
         }
     }
 
     public String get(String key) {
         int hash = this.hash(key);
-        LinkedList keyList = this.table.get(hash);
+        LinkedList<String> keyList = this.table.get(hash);
 
         if(keyList == null) return null;
         else {
             for(String s : keyList) {
-                String[] arr = s.split(':');
+                String[] arr = s.split(":");
                 if(arr[0].equals(key)) return arr[1];
             }
             return null;
