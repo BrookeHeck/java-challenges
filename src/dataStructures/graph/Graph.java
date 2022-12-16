67a0ffcbbc49431e5021be49af31cfebdb471ad0
BrookeHeck <b19heck@gmail.com>
2022-12-15 17:06:02 -0800

    add graph skeleton

--- src/dataStructures/graph/Graph.java
+++ src/dataStructures/graph/Graph.java
@@ -1,12 +1,41 @@
 package dataStructures.graph;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 
-public class Graph {
-    HashMap<String, String> adjacencies;
+public class Graph<Vertex> {
+    HashMap<String, ArrayList<Vertex>> adjacencies;
 
     public Graph() {
         adjacencies = new HashMap<>();
+    }
+
+    public Vertex addVertex(String value) {
+
+    }
+
+    public void addEdge(Vertex startVertex, Vertex endVertex) {
+
+    }
+
+    public void addEdge(Vertex starVertex, Vertex endVertex, int weight) {
+
+    }
+
+    public ArrayList<Vertex> getNeighbors(Vertex vertex) {
+
+    }
+
+    public int getDegree(Vertex vertex) {
+
+    }
+
+    public ArrayList<Vertex> getVertices() {
+
+    }
+
+    public int getSize() {
+        
     }
 
 }
