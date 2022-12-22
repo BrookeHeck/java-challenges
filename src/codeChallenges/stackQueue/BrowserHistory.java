8f7cd2a2c820840dca413c487ca24a04c9b57dd8
BrookeHeck <b19heck@gmail.com>
2022-12-22 15:30:02 -0600

    browser history challenge

--- src/codeChallenges/stackQueue/BrowserHistory.java
+++ src/codeChallenges/stackQueue/BrowserHistory.java
@@ -1,5 +1,36 @@
 package codeChallenges.stackQueue;
 
+import dataStructures.Stack.*;
+
 public class BrowserHistory {
+    Stack previous;
+    Stack forward;
+    
+    String current;
+
+    public BrowserHistory() {
+        this.previous = new Stack();
+        this.forward = new Stack();
+        this.current = "";
+    }
+
+    public void changePage(String currentUrl) {
+        this.previous.push(this.current);
+        this.current = currentUrl;
+    }
+
+    public String goBack() {
+        String temp = this.current;
+        this.forward.push(temp);
+        this.current = this.previous.pop();
+        return temp;
+    }
+
+    public String goForward() {
+        String temp = this.current;
+        this.previous.push(temp);
+        this.current = this.forward.pop();
+        return temp;
+    }
     
 }
