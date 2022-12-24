5b53a32b9f9a3324c5d0fd537ff27b6718782927
BrookeHeck <b19heck@gmail.com>
2022-12-24 15:06:58 -0600

    kary tree structure

--- src/dataStructures/KAryTree/KAryTree.java
+++ src/dataStructures/KAryTree/KAryTree.java
@@ -1,5 +1,50 @@
 package dataStructures.KAryTree;
 
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.LinkedList;;
+
+class Node {
+    String value;
+    ArrayList<Node> children;
+
+    public Node(String value) {
+        this.value = value;
+        this.children = new  ArrayList<>();
+    }
+
+    public void addChild(Node node) {
+        this.children.add(node);
+    }
+}
+
 public class KAryTree {
+    Node root;
+    int k;
+
+    public KAryTree(int k, String value) {
+        this.k = k;
+        this.root = new Node(value);
+    }
+
+    public void add(String value) {
+        Queue<Node> queue = new LinkedList<>();
+        boolean isAdded = false;
+        Node current = this.root;
+
+        while(current != null && !isAdded) {
+            if(current.children.size() < k) {
+                current.addChild(new Node(value));
+                isAdded = true;
+            } else {
+                for(Node node : current.children ) {
+                    queue.add(node);
+                }
+            }
+
+            current = queue.poll();
+        }
+
+    }
     
 }
